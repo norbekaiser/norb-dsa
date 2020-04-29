4c314ad4ad6a756f54ec2903e2e7c220f3d75020
Norbert L. Rühl <git@norbert-ruehl.de>
2020-04-29 14:26:16 +0200

    updated license information in other fiels

--- examples/ToplogicalSort/main.cc
+++ examples/ToplogicalSort/main.cc
@@ -1,4 +1,14 @@
-
+// Copyright (c) 2020 Norbert Rühl
+//
+// This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+//
+//     1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+//
+//     2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+//
+//     3. This notice may not be removed or altered from any source distribution.
 
 #include <iostream>
 
@@ -9,7 +19,7 @@ int main(int argc, char** argv)
     using std::cout;
     using std::endl;
     cout << "Generating Elements" << endl;
-    std::string Schuhe="Schuhe",Socken="Socken",Mantel="Mantel",Hose="Hose",Unterhose="Unterhose",Pullover="Pullover",Unterhemd="Unterhemd";
+    std::string Schuhe="Schuhe",Socken="Socken",Mantel="Mantel",Hose="Hose",Unterhose="Unterhose",Pullover="Pullover",Unterhemd="Unterhemd",Brille="Brille";
 
     Graph<std::string> G;
     cout << "Adding Elements to Graph" << endl;
@@ -22,7 +32,7 @@ int main(int argc, char** argv)
     auto g_ptr_hose = G.addToGraph(Hose);
     auto g_ptr_schuhe = G.addToGraph(Schuhe);
     auto g_ptr_mantel = G.addToGraph(Mantel);
-
+    //don't forget your glasses
 
     cout << "Connecting Nodes as they were all lonely" << endl;
     g_ptr_unterhemd->addEdge(g_ptr_pullover);
