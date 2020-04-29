06c3ecb162b50ee56ea92f369d38b1d54315c997
Norbert L. Rühl <git@norbert-ruehl.de>
2020-04-29 10:18:50 +0200

    added examples, also browke, DepthFirstSearch

---
+++ examples/TestDepthSearch/main.cc
@@ -0,0 +1,47 @@
+
+
+#include <iostream>
+
+#include "Graph/Graph.hh"
+#include "Graph/DepthFirstSearch.hh"
+
+int main(int argc, char** argv)
+{
+    using std::cout;
+    using std::endl;
+    cout << "Generating Elements" << endl;
+    char u,v,x,y,w,z;
+    u='u';v='v';x='x';y='y';w='w';z='z';
+    Graph<decltype(u)> G;
+    cout << "Adding Elements to Graph" << endl;
+    auto g_ptr_u = G.addToGraph(u);
+    auto g_ptr_v = G.addToGraph(v);
+    auto g_ptr_w = G.addToGraph(w);
+    auto g_ptr_x = G.addToGraph(x);
+    auto g_ptr_y = G.addToGraph(y);
+    auto g_ptr_z = G.addToGraph(z);
+    cout << "Connecting Nodes as they were all lonely" << endl;
+    //u -> v
+    g_ptr_u->addEdge(g_ptr_v);
+    //u -> x
+    g_ptr_u->addEdge(g_ptr_x);
+    //v -> y
+    g_ptr_v->addEdge(g_ptr_y);
+    //y -> x
+    g_ptr_y->addEdge(g_ptr_x);
+    //x -> v
+    g_ptr_x->addEdge(g_ptr_v);
+    //w -> z
+    g_ptr_w->addEdge(g_ptr_z);
+    //w -> y
+    g_ptr_w->addEdge(g_ptr_y);
+    //z -> z
+    g_ptr_z->addEdge(g_ptr_z);
+    cout << "Depth First Search" << endl;
+    DepthFirstSearch<decltype(u)>(G);
+
+    return 0;
+
+
+
+}
---
+++ examples/TestTopologicalSort/main.cc
@@ -0,0 +1,44 @@
+
+
+#include <iostream>
+
+#include "Graph/Graph.hh"
+#include "Graph/ToplogicalSort.hh"
+int main(int argc, char** argv)
+{
+    using std::cout;
+    using std::endl;
+    cout << "Generating Elements" << endl;
+    std::string Schuhe="Schuhe",Socken="Socken",Mantel="Mantel",Hose="Hose",Unterhose="Unterhose",Pullover="Pullover",Unterhemd="Unterhemd";
+
+    Graph<std::string> G;
+    cout << "Adding Elements to Graph" << endl;
+
+    auto g_ptr_unterhemd = G.addToGraph(Unterhemd);
+    auto g_ptr_pullover = G.addToGraph(Pullover);
+    auto g_ptr_mantel = G.addToGraph(Mantel);
+    auto g_ptr_unterhose = G.addToGraph(Unterhose);
+    auto g_ptr_hose = G.addToGraph(Hose);
+    auto g_ptr_socken = G.addToGraph(Socken);
+    auto g_ptr_schuhe = G.addToGraph(Schuhe);
+
+
+    cout << "Connecting Nodes as they were all lonely" << endl;
+    g_ptr_unterhemd->addEdge(g_ptr_pullover);
+    g_ptr_pullover->addEdge(g_ptr_mantel);
+    g_ptr_unterhose->addEdge(g_ptr_hose);
+    g_ptr_hose->addEdge(g_ptr_mantel);
+    g_ptr_hose->addEdge(g_ptr_schuhe);
+    g_ptr_socken->addEdge(g_ptr_schuhe);
+    cout << "Sorting Topologically" << endl;
+    auto List = TopologicalSort<std::string>(G);
+    cout << "Let's see how strange we dress us today" << endl;
+    for(auto &Node: List)
+    {
+        cout << Node->getElem() << endl;
+    }
+    return 0;
+
+
+
+}
